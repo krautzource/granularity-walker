dedef4c0be5eeed2f6b3acdcbf2f166e2e3640a0
Peter Krautzberger <p.krautzberger@gmail.com>
2023-06-05 22:03:28 +0200

    refactor: "moves" as module
    Separates out moves-related code to module.
    * index.html: switch to type=module
    * index.js
      * import moveUp/Down/Horizontally()
      * (re)move add/clearRect()
      * (re)move moveUp/Down/Horizontally()
    * add move.js
      * helpers: add/clearRect()
      * exports: moveUp/Down/Horizontally()

---
+++ moves.js
@@ -0,0 +1,69 @@
+function addRect(node) {
+    console.log("addRect:", node);
+    if (node.tagName !== "g") return;
+    const bbox = node.getBBox();
+    node.insertAdjacentHTML(
+      "afterbegin",
+      `<rect x="${bbox.x}" y="${bbox.y}" width="${bbox.width}" height="${bbox.height}" data-rect="true" fill="transparent" stroke="blue"/>`
+    );
+  }
+  
+  function clearRect(node) {
+    console.log("clearRect:", node);
+    if (node.tagName !== "g") return;
+    node.querySelectorAll("rect[data-rect]").forEach((node) => node.remove());
+  }
+  
+  
+export function moveDown(activeNode) {
+    const activeNodeLevel = parseInt(activeNode.getAttribute("data-level"));
+    const newLevel = activeNodeLevel + 1;
+    const newActiveNode = activeNode.querySelector(`[data-level="${newLevel}"]`);
+    if (!newActiveNode) return;
+    activeNode.removeAttribute("tabindex");
+    activeNode.removeAttribute("aria-label");
+    activeNode.setAttribute("role", "none");
+    clearRect(activeNode);
+    activeNode
+        .querySelectorAll(`[data-level="${newLevel}"]`)
+        .forEach((currentLevelChild) => {
+            currentLevelChild.setAttribute("tabindex", "0");
+            currentLevelChild.removeAttribute("aria-hidden");
+            currentLevelChild.setAttribute(
+                "aria-label",
+                currentLevelChild.getAttribute("data-label")
+            );
+            currentLevelChild.setAttribute("role", "img");
+            addRect(currentLevelChild);
+        });
+    newActiveNode.focus();
+    return newActiveNode;
+}
+
+export function moveUp(activeNode) {
+    const activeNodeLevel = parseInt(activeNode.getAttribute("data-level"));
+    if (activeNodeLevel < 1) return;
+    const newLevel = Math.max(activeNodeLevel - 1, 0);
+    const ancestor = activeNode.closest(`[data-level="${newLevel}"]`);
+    ancestor.setAttribute("tabindex", "0");
+    ancestor.setAttribute("aria-label", ancestor.getAttribute("data-label"));
+    ancestor.setAttribute("role", "img");
+    addRect(ancestor);
+    ancestor.querySelectorAll('[tabindex="0"]').forEach((descendant) => {
+        descendant.removeAttribute("tabindex");
+        descendant.setAttribute("aria-hidden", "true");
+        descendant.removeAttribute("aria-label");
+        descendant.setAttribute("role", "none");
+        clearRect(descendant);
+    });
+    ancestor.focus();
+    return ancestor;
+}
+
+export function moveHorizontally(activeNode, isReversed) {
+    const increment = isReversed ? -1 : 1;
+    const currentNodes = [
+        ...activeNode.closest('[data-level="0"]').querySelectorAll('[tabindex="0"]')
+    ];
+    currentNodes[currentNodes.indexOf(activeNode) + increment]?.focus();
+}
